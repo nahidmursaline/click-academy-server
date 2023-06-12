d0a8f1786b54c40c4d710c6bda86c6bf6934767e
nahidmursaline <nahid15-12408@diu.edu.bd>
2023-06-12 17:00:56 +0600

    delete

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ltkvgnf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -43,6 +43,25 @@ async function run() {
         const item = req.body;
         const result = await cartCollection.insertOne(item);
         res.send(result);
+    })
+
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email;
+      if(!email){
+        res.send([]);
+      }
+      else{
+        const query = {email: email};
+        const result = await cartCollection.find(query).toArray();
+        res.send(result)
+      }
+    })
+
+    app.delete('/carts/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
+      res.send(result)
     })
 
 
